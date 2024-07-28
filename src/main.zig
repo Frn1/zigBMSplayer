49cf8b2bce5434111112c844f54d97e3eddffe8b
Frn1 <torrescontarini@gmail.com>
2024-07-28 01:24:57 -0300

    Implement modular system for the conductor and implement bpm changes (with debug visualization)

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,15 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
+const c = @import("consts.zig");
+const formats = @import("formats.zig");
+const gfx = @import("graphics.zig");
+const utils = @import("utils.zig");
+const audio = @import("audio.zig");
+
+const Conductor = @import("rhythm/conductor.zig").Conductor;
+const Object = @import("rhythm/object.zig").Object;
+
 const sdl = @cImport({
     @cInclude("SDL2/SDL.h");
     @cInclude("SDL2/SDL_ttf.h");
@@ -11,16 +20,9 @@ const ma = @cImport({
     @cInclude("miniaudio.h");
 });
 
-const c = @import("consts.zig");
-const rhythm = @import("rhythm.zig");
-const formats = @import("formats.zig");
-const gfx = @import("graphics.zig");
-const utils = @import("utils.zig");
-const audio = @import("audio.zig");
-
 pub fn main() !void {
     // --- Initialization ---
-    
+
     // Main allocator we use
     // We use GeneralPurposeAllocator in debug, C Allocator in release
     var main_allocator: std.mem.Allocator = undefined;
@@ -36,11 +38,11 @@ pub fn main() !void {
         //fail test; can't try in defer as defer is executed after we return
         if (deinit_status == .leak) @panic("Memory leak :(");
     };
-    
+
     var loading_arena = std.heap.ArenaAllocator.init(main_allocator);
     // Allocator used when loading stuff that wont stay after loading
     const loading_allocator = loading_arena.allocator();
-    
+
     // init sdl
     try utils.sdlAssert(
         sdl.SDL_Init(@intCast(sdl.SDL_INIT_TIMER | sdl.SDL_INIT_VIDEO | sdl.SDL_INIT_EVENTS)) == 0,
@@ -72,7 +74,7 @@ pub fn main() !void {
     const exe_folder_path = try std.fs.selfExeDirPathAlloc(loading_allocator);
     defer loading_allocator.free(exe_folder_path);
     const exe_dir = try std.fs.openDirAbsolute(exe_folder_path, .{});
-    
+
     const debug_font_path = a: {
         // Literally just realpathAlloc but using realpathZ for SDL2 compatability
         var buf: [std.fs.max_path_bytes]u8 = undefined;
@@ -85,10 +87,10 @@ pub fn main() !void {
     try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF) == 0);
     try utils.sdlAssert(sdl.SDL_RenderClear(renderer) == 0);
     sdl.SDL_RenderPresent(renderer);
-    
+
     try gfx.drawText("SDL initialized", renderer, 0, 24 * 0, debug_font);
     sdl.SDL_RenderPresent(renderer);
-    
+
     // init miniaudio
     const ma_engine: *ma.ma_engine = try main_allocator.create(ma.ma_engine);
     defer main_allocator.destroy(ma_engine);
@@ -101,12 +103,16 @@ pub fn main() !void {
         return error.MiniaudioError; // Failed to initialize the engine.
     }
     defer ma.ma_engine_uninit(ma_engine);
-    
+
     try gfx.drawText("Miniaudio initialized", renderer, 0, 24 * 2, debug_font);
     sdl.SDL_RenderPresent(renderer);
 
-    var scroll_speed_mul: f80 = 2.0;
-    
+    var scroll_speed_mul: Object.Position = 2.0;
+
+    var loading_arena = std.heap.ArenaAllocator.init(main_allocator);
+    // Allocator used when loading stuff that wont stay after loading
+    const loading_allocator = loading_arena.allocator();
+
     const args = try std.process.argsAlloc(loading_allocator);
     if (args.len < 2) {
         std.log.err("ERROR: Missing path\n", .{});
@@ -130,7 +136,7 @@ pub fn main() !void {
         return e;
     };
     defer chart_file.close();
-    
+
     try gfx.drawText("BMS file read - Compiling now...", renderer, 0, 24 * 4, debug_font);
     sdl.SDL_RenderPresent(renderer);
 
@@ -152,16 +158,16 @@ pub fn main() !void {
         // std.process.exit(1);
     };
     // Make sure to unload the keysounds
-    defer for (conductor.keysounds) |sound| {
-        if (sound != null) {
-            ma.ma_sound_uninit(sound);
-            main_allocator.destroy(sound.?);
-        }
-    };
+    // defer for (conductor.keysounds) |sound| {
+    //     if (sound != null) {
+    //         ma.ma_sound_uninit(sound);
+    //         main_allocator.destroy(sound.?);
+    //     }
+    // };
 
     // Make sure to free the chart data
-    defer main_allocator.free(conductor.notes);
-    defer main_allocator.free(conductor.segments);
+    // defer main_allocator.free(conductor.notes);
+    // defer main_allocator.free(conductor.segments);
 
     try gfx.drawText("BMS file read - Compiling now... Done!", renderer, 0, 24 * 4, debug_font);
     sdl.SDL_RenderPresent(renderer);
@@ -172,33 +178,33 @@ pub fn main() !void {
     loading_arena.deinit();
 
     // Create objects for the conductor
-    try conductor.createObjects(main_allocator);
-    defer conductor.deleteObjects(main_allocator);
+    // try conductor.createObjects(main_allocator);
+    // defer conductor.deleteObjects(main_allocator);
 
     try gfx.drawText("Objects created", renderer, 0, 24 * 5, debug_font);
     sdl.SDL_RenderPresent(renderer);
 
     // Calculate time we hit every object
-    const object_times = try conductor.calculateObjectTimesInSeconds(main_allocator);
+    const object_times = try conductor.calculateSecondsAlloc(main_allocator);
     defer main_allocator.free(object_times);
     // Calculate position we hit every object
-    const object_positions = try conductor.calculateVisualBeats(main_allocator);
+    const object_positions = try conductor.calculatePositionsAlloc(main_allocator);
     defer main_allocator.free(object_positions);
 
     try gfx.drawText("Object times and positions calculated", renderer, 0, 24 * 6, debug_font);
     sdl.SDL_RenderPresent(renderer);
 
     // --- Game loop ---
-    
+
     // the start of the performance tick counter
     const start_tick = sdl.SDL_GetPerformanceCounter();
-    
+
     // how many ticks are in a second
     const performance_frequency: f80 = @floatFromInt(sdl.SDL_GetPerformanceFrequency());
 
     // used for fps
     var last_frame_end = sdl.SDL_GetPerformanceCounter();
-    
+
     var audio_stop_flag = false;
     const audioThread = try std.Thread.spawn(.{ .allocator = main_allocator }, audio.audioThread, .{
         &conductor,
@@ -250,21 +256,21 @@ pub fn main() !void {
         const current_time: f80 = @as(f80, @floatFromInt(current_performance_ticks)) / performance_frequency;
 
         // update game state
-        state.process(conductor, current_time);
+        state.update(conductor, current_time, true);
 
-        if (state.next_object_to_process == conductor.objects.len - 1) {
-            for (0..1295) |channel| {
-                if (conductor.keysounds[channel] != null) {
-                    if (ma.ma_sound_is_playing(conductor.keysounds[channel]) == ma.MA_TRUE) {
-                        break; // If a sound is still playing, break the inner loop so we dont quit
-                    }
-                }
-            } else {
-                break :main_loop; // Quit the program (the outer loop)
-            }
-        }
+        // if (state.next_object_to_process == conductor.objects.len - 1) {
+        //     for (0..1295) |channel| {
+        //         if (conductor.keysounds[channel] != null) {
+        //             if (ma.ma_sound_is_playing(conductor.keysounds[channel]) == ma.MA_TRUE) {
+        //                 break; // If a sound is still playing, break the inner loop so we dont quit
+        //             }
+        //         }
+        //     } else {
+        //         break :main_loop; // Quit the program (the outer loop)
+        //     }
+        // }
 
-        const visual_beat = state.calculateVisualPosition(state.current_beat);
+        const visual_position = state.calculateVisualPosition(state.current_beat);
 
         defer last_frame_end = sdl.SDL_GetPerformanceCounter();
         defer sdl.SDL_RenderPresent(renderer);
@@ -272,117 +278,124 @@ pub fn main() !void {
         try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF) == 0);
         try utils.sdlAssert(sdl.SDL_RenderClear(renderer) == 0);
 
-        // Draw barlines BEFORE notes so they appear behind the notes
-        for (conductor.objects, object_positions, 0..) |object, position, i| {
-            var render_y = @as(i32, @intFromFloat((visual_beat - position.visual_beat) * scroll_speed_mul * c.beat_height));
-            render_y += c.judgement_line_y;
-
-            if (render_y < -c.note_height) {
-                continue;
-            }
-
-            if (object.obj_type == .Segment) {
-                if (render_y > c.screen_height + c.note_height) {
-                    continue;
-                }
-
-                // change color to white
-                try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF) == 0);
-
-                const segment = conductor.segments[conductor.objects[i].index];
-                switch (segment.type) {
-                    .barline => {
-                        try utils.sdlAssert(sdl.SDL_RenderDrawLine(
-                            renderer,
-                            0,
-                            render_y,
-                            gfx.getXForLane(
-                                @as(u7, switch (conductor.chart_type) {
-                                    .beat5k => 5 + 1,
-                                    .beat7k => 7 + 1,
-                                    .beat10k => 10 + 2,
-                                    .beat14k => 14 + 2,
-                                }),
-                            ),
-                            render_y,
-                        ) == 0);
-                    },
-                    else => {},
-                }
+        for (conductor.objects, object_positions, 0..) |object, object_position, i| {
+            _ = i;
+            if (object.render != null) {
+                try object.render.?(object, visual_position, object_position, scroll_speed_mul, false, renderer);
             }
         }
 
-        // And NOW we draw the notes
-        for (conductor.objects, object_positions, 0..) |object, position, i| {
-            var render_y = @as(i32, @intFromFloat((visual_beat - position.visual_beat) * scroll_speed_mul * c.beat_height));
-            render_y += c.judgement_line_y;
+        // // Draw barlines BEFORE notes so they appear behind the notes
+        // for (conductor.objects, object_positions, 0..) |object, position, i| {
+        //     var render_y = @as(i32, @intFromFloat((visual_beat - position.visual_beat) * scroll_speed_mul * c.beat_height));
+        //     render_y += c.judgement_line_y;
 
-            if (render_y < -c.note_height) {
-                continue;
-            }
+        //     if (render_y < -c.note_height) {
+        //         continue;
+        //     }
 
-            if (object.obj_type == rhythm.Conductor.ObjectType.Note) {
-                const note = conductor.notes[conductor.objects[i].index];
+        //     if (object.obj_type == .Segment) {
+        //         if (render_y > c.screen_height + c.note_height) {
+        //             continue;
+        //         }
 
-                // TODO: PMS detection and support
-                const lane = switch (note.lane) {
-                    36 => 15,
-                    37...45 => note.lane - 29,
-                    else => note.lane,
-                };
+        //         // change color to white
+        //         try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF) == 0);
 
-                const render_x = gfx.getXForLane(lane);
+        //         const segment = conductor.segments[conductor.objects[i].index];
+        //         switch (segment.type) {
+        //             .barline => {
+        //                 try utils.sdlAssert(sdl.SDL_RenderDrawLine(
+        //                     renderer,
+        //                     0,
+        //                     render_y,
+        //                     gfx.getXForLane(
+        //                         @as(u7, switch (conductor.chart_type) {
+        //                             .beat5k => 5 + 1,
+        //                             .beat7k => 7 + 1,
+        //                             .beat10k => 10 + 2,
+        //                             .beat14k => 14 + 2,
+        //                         }),
+        //                     ),
+        //                     render_y,
+        //                 ) == 0);
+        //             },
+        //             else => {},
+        //         }
+        //     }
+        // }
 
-                // set lane color
-                const color = gfx.getColorForLane(lane);
-                try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a) == 0);
+        // // And NOW we draw the notes
+        // for (conductor.objects, object_positions, 0..) |object, position, i| {
+        //     var render_y = @as(i32, @intFromFloat((visual_beat - position.visual_beat) * scroll_speed_mul * c.beat_height));
+        //     render_y += c.judgement_line_y;
 
-                switch (note.type) {
-                    .normal => {
-                        if (render_y > c.screen_height) {
-                            continue;
-                        }
+        //     if (render_y < -c.note_height) {
+        //         continue;
+        //     }
 
-                        switch (note.type.normal) {
-                            .mine => {
-                                // change color to yellow
-                                try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0x00, 0xFF) == 0);
-                            },
-                            .hidden => {
-                                // change color to green
-                                try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, 0x00, 0xA0, 0x00, 0xFF) == 0);
-                            },
-                            else => {},
-                        }
+        //     if (object.obj_type == .Note) {
+        //         const note = conductor.notes[conductor.objects[i].index];
 
-                        const note_rect: sdl.SDL_Rect = sdl.SDL_Rect{
-                            .x = render_x,
-                            .y = render_y - c.note_height,
-                            .w = gfx.getWidthForLane(lane),
-                            .h = c.note_height,
-                        };
-                        try utils.sdlAssert(sdl.SDL_RenderFillRect(renderer, &note_rect) == 0);
-                    },
-                    .ln_head => {
-                        var tail_render_y = @as(i32, @intFromFloat(
-                            (visual_beat - object_positions[position.ln_tail_obj_index.?].visual_beat) * scroll_speed_mul * c.beat_height,
-                        ));
-                        tail_render_y += c.judgement_line_y;
-                        if (tail_render_y > c.screen_height) {
-                            continue;
-                        }
-                        const note_rect: sdl.SDL_Rect = sdl.SDL_Rect{
-                            .x = render_x,
-                            .y = tail_render_y,
-                            .w = gfx.getWidthForLane(lane),
-                            .h = render_y - tail_render_y,
-                        };
-                        try utils.sdlAssert(sdl.SDL_RenderFillRect(renderer, &note_rect) == 0);
-                    },
-                    else => {},
-                }
-            }
-        }
+        //         // TODO: PMS detection and support
+        //         const lane = switch (note.lane) {
+        //             36 => 15,
+        //             37...45 => note.lane - 29,
+        //             else => note.lane,
+        //         };
+
+        //         const render_x = gfx.getXForLane(lane);
+
+        //         // set lane color
+        //         const color = gfx.getColorForLane(lane);
+        //         try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a) == 0);
+
+        //         switch (note.type) {
+        //             .normal => {
+        //                 if (render_y > c.screen_height) {
+        //                     continue;
+        //                 }
+
+        //                 switch (note.type.normal) {
+        //                     .mine => {
+        //                         // change color to yellow
+        //                         try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0x00, 0xFF) == 0);
+        //                     },
+        //                     .hidden => {
+        //                         // change color to green
+        //                         try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, 0x00, 0xA0, 0x00, 0xFF) == 0);
+        //                     },
+        //                     else => {},
+        //                 }
+
+        //                 const note_rect: sdl.SDL_Rect = sdl.SDL_Rect{
+        //                     .x = render_x,
+        //                     .y = render_y - c.note_height,
+        //                     .w = gfx.getWidthForLane(lane),
+        //                     .h = c.note_height,
+        //                 };
+        //                 try utils.sdlAssert(sdl.SDL_RenderFillRect(renderer, &note_rect) == 0);
+        //             },
+        //             .ln_head => {
+        //                 var tail_render_y = @as(i32, @intFromFloat(
+        //                     (visual_beat - object_positions[position.ln_tail_obj_index.?].visual_beat) * scroll_speed_mul * c.beat_height,
+        //                 ));
+        //                 tail_render_y += c.judgement_line_y;
+        //                 if (tail_render_y > c.screen_height) {
+        //                     continue;
+        //                 }
+        //                 const note_rect: sdl.SDL_Rect = sdl.SDL_Rect{
+        //                     .x = render_x,
+        //                     .y = tail_render_y,
+        //                     .w = gfx.getWidthForLane(lane),
+        //                     .h = render_y - tail_render_y,
+        //                 };
+        //                 try utils.sdlAssert(sdl.SDL_RenderFillRect(renderer, &note_rect) == 0);
+        //             },
+        //             else => {},
+        //         }
+        //     }
+        // }
 
         // change color to white
         try utils.sdlAssert(sdl.SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF) == 0);
@@ -406,17 +419,22 @@ pub fn main() !void {
         // draw text used for debuging
         const text: [:0]u8 = try frame_allocator.allocSentinel(u8, 64, 0);
 
-        _ = try std.fmt.bufPrint(text, "B   {d:.3}   {d:.3}\u{0000}", .{ state.current_beat, 60.0 / state.current_sec_per_beat });
+        _ = try std.fmt.bufPrint(text, "B   {d:.3}   {d:.3}\u{0000}", .{
+            state.current_beat,
+            60.0 / state.seconds_per_beat,
+        });
 
         try gfx.drawText(text, renderer, 0, 24 * 0, debug_font);
 
-        _ = try std.fmt.bufPrint(text, "VB  {d:.3}  x{d:.3}\u{0000}", .{ visual_beat, state.current_scroll_mul });
-        try gfx.drawText(text, renderer, 0, 24 * 1, debug_font);
+        // _ = try std.fmt.bufPrint(text, "VB  {d:.3}  x{d:.3}\u{0000}", .{ visual_beat, state.current_scroll_mul });
+        // try gfx.drawText(text, renderer, 0, 24 * 1, debug_font);
 
-        _ = try std.fmt.bufPrint(text, "FPS {d:.3}\u{0000}", .{performance_frequency / @as(
-            f80,
-            @floatFromInt(sdl.SDL_GetPerformanceCounter() - last_frame_end),
-        )});
+        _ = try std.fmt.bufPrint(text, "FPS {d:.3}\u{0000}", .{
+            performance_frequency / @as(
+                f80,
+                @floatFromInt(sdl.SDL_GetPerformanceCounter() - last_frame_end),
+            ),
+        });
         try gfx.drawText(text, renderer, 0, 24 * 2, debug_font);
 
         _ = try std.fmt.bufPrint(text, "SC  {d:.1}\u{0000}", .{scroll_speed_mul});
---
+++ src/rhythm/object.zig
@@ -0,0 +1,87 @@
+const std = @import("std");
+
+const State = @import("conductor.zig").Conductor.State;
+
+const Renderer = @cImport({
+    @cInclude("SDL2/SDL.h");
+    @cInclude("SDL2/SDL_ttf.h");
+}).SDL_Renderer;
+
+pub const Object = struct {
+    pub const Time = f80;
+    pub const Position = f32;
+
+    /// Rhythmic time that the object would be hit at
+    beat: Time,
+
+    /// Priority used for sorting when objects have the same `beat`
+    ///
+    /// Lower comes first while higher comes after
+    priority: i8 = 0,
+
+    /// Extra parameters for this object
+    parameters: *anyopaque = undefined,
+
+    /// Pointer to a function to initialize `parameters`
+    /// and do other work before starting the game.
+    ///
+    /// Called when loading
+    ///
+    /// **MUST BE MULTI-THREADING SAFE**
+    init: ?*const fn (self: *@This(), allocator: std.mem.Allocator) error{
+        OutOfMemory,
+        InvalidParameters,
+    }!void = null,
+
+    /// Pointer to a function to destroy `parameters`
+    /// and everything created in init
+    ///
+    /// Called when exiting
+    ///
+    /// **MUST BE MULTI-THREADING SAFE**
+    destroy: ?*const fn (self: *@This(), allocator: std.mem.Allocator) void = null,
+
+    /// Called when processing/loading/running gameplay.
+    /// Will run at the "perfect" time for the object.
+    ///
+    /// For example, a BPM object would change the bpm in here,
+    /// while a BGM note object would do nothing in here (so it should be null).
+    process: ?*const fn (self: @This(), state: *State) void = null,
+
+    /// Called when running gameplay.
+    /// Will run at the "perfect" time for the object.
+    ///
+    /// For example, a BPM object would do nothing in here (so it should be null),
+    /// while a BGM note object would play their keysound.
+    processGameplay: ?*const fn (self: @This()) void = null,
+
+    /// Called when running gameplay.
+    /// Will run at the time the player hits this object.
+    ///
+    /// TODO: Ratings
+    hit: ?*const fn (
+        self: @This(),
+        perfect_hit_time: Object.Time,
+        actual_hit_time: Object.Time,
+    ) void = null,
+
+    /// Called when running gameplay.
+    /// Will render the object on screen.
+    // TODO: Make this better probably
+    render: ?*const fn (
+        self: @This(),
+        object_position: Object.Position,
+        current_position: Object.Position,
+        scroll_speed: Object.Position,
+        scroll_direction: enum { Up, Down },
+        renderer: *Renderer,
+    ) error{SdlError}!void = null,
+
+    /// Use this function for sorting lists of objects
+    pub fn lessThanFn(_: void, lhs: @This(), rhs: @This()) bool {
+        if (lhs.beat == rhs.beat) {
+            return lhs.priority < rhs.priority;
+        }
+        return lhs.beat < rhs.beat;
+    }
+};
